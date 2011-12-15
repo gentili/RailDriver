01be1a8a19779299e3b294e26d61ef3f013c17fd
Michael Gentili <gentili@mcpnet.ca>
2011-12-15 17:25:23 -0500

    Create initial Maven Project

---
+++ src/main/java/ca/mcpnet/RailDriver/App.java
@@ -0,0 +1,13 @@
+package ca.mcpnet.RailDriver;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
