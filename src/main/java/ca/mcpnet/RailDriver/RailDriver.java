4c428c6c59f045a8e4b80980131892380d5bf935
Michael Gentili <gentili@mcpnet.ca>
2012-11-22 21:37:35 -0500

    Update to work with CraftBukkit 1.4.5.

--- src/main/java/ca/mcpnet/RailDriver/RailDriver.java
+++ src/main/java/ca/mcpnet/RailDriver/RailDriver.java
@@ -24,7 +24,7 @@ import org.bukkit.util.Vector;
 
 public class RailDriver extends JavaPlugin {
 
-	static public final String VERSION = "0.2";
+	static public final String VERSION = "0.3";
 	static Logger logger = Logger.getLogger("Minecraft");
 	static public void log(String msg) {
 		logger.info("[RailDriver] "+msg);
